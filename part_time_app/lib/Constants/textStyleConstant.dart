8ca812d96c6cb6ab82c8dadac5c63ebb164fb7ba
MindarkDaniel <jialexiong777@gmail.com>
2024-04-26 13:43:15 +0800

    create third status selection component

--- part_time_app/lib/Constants/textStyleConstant.dart
+++ part_time_app/lib/Constants/textStyleConstant.dart
@@ -9,3 +9,14 @@ const selectedSecondaryTitleTextStyle = TextStyle(
     fontSize: 22, fontWeight: FontWeight.w700, color: kMainBlackColor);
 const unselectedSecondaryTitleTextStyle = TextStyle(
     fontSize: 16, fontWeight: FontWeight.w600, color: kMainBlackColor);
+
+const selectedThirdStatusTextStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: kMainBlackColor,
+    height: 1);
+const unselectedThirdStatusTextStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: kMainGreyColor,
+    height: 1);
--- part_time_app/lib/Pages/ComponentDisplayPage/componentExamplePage.dart
+++ part_time_app/lib/Pages/ComponentDisplayPage/componentExamplePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../Components/Selection/thirdStatusSelectionComponent.dart';
 import '../../Components/Title/secondaryTitleComponent.dart';
 
 class ComponentExample extends StatefulWidget {
@@ -19,9 +20,20 @@ class _ComponentExampleState extends State<ComponentExample> {
         body: SingleChildScrollView(
           padding: EdgeInsets.all(10),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SecondaryTitleComponent(
                 titleList: ["推荐", "收藏", "我接收的", "我发布的", "发布悬赏"],
+                selectedIndex: selectIndex,
+                onTap: (index) {
+                  setState(() {
+                    selectIndex = index;
+                  });
+                },
+              ),
+              ThirdStatusSelectionComponent(
+                statusList: ["待完成", "待审核", "未通过", "待到账", "已到账"],
                 selectedIndex: selectIndex,
                 onTap: (index) {
                   setState(() {
