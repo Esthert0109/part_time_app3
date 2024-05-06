0b753970a7d7fe5652e2b7526e384cc7eef74cba
MindarkDaniel <jialexiong777@gmail.com>
2024-05-06 20:06:54 +0800

    create recipient info page

--- part_time_app/lib/Pages/Message/messageMainPage.dart
+++ part_time_app/lib/Pages/Message/messageMainPage.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:part_time_app/Components/Button/primaryButtonComponent.dart';
 import 'package:part_time_app/Components/Card/missionPublishCheckoutCardComponent.dart';
-import 'package:part_time_app/Components/TextField/primaryTextFieldComponent.dart';
-import 'package:part_time_app/Components/TextField/secondaryTextFieldComponent.dart';
 import 'package:part_time_app/Pages/UserAuth/changePassword.dart';
 import 'package:part_time_app/Pages/UserAuth/loginPage.dart';
 import 'package:part_time_app/Pages/UserAuth/otpCode.dart';
--- part_time_app/lib/Pages/MissionRecipient/recipientInfoPage.dart
+++ part_time_app/lib/Pages/MissionRecipient/recipientInfoPage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:part_time_app/Components/Button/primaryButtonComponent.dart';
+import 'package:part_time_app/Components/TextField/secondaryTextFieldComponent.dart';
 
 import '../../Components/Title/thirdTitleComponent.dart';
 import '../../Constants/colorConstant.dart';
+import '../../Constants/textStyleConstant.dart';
 
 class RecipientInfoPage extends StatefulWidget {
   const RecipientInfoPage({super.key});
@@ -12,6 +16,9 @@ class RecipientInfoPage extends StatefulWidget {
 }
 
 class _RecipientInfoPageState extends State<RecipientInfoPage> {
+  TextEditingController addressController = TextEditingController();
+  TextEditingController networkController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -49,14 +56,101 @@ class _RecipientInfoPageState extends State<RecipientInfoPage> {
               stops: [0.0, 0.15],
             ),
           ),
-          child: Container(
-            height: 201,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                color: kMainWhiteColor, borderRadius: BorderRadius.circular(8)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: kMainWhiteColor,
+                  borderRadius: BorderRadius.circular(8)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 6),
+                    child: Text(
+                      "钱包地址",
+                      style: messageText1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: secondaryTextFieldComponent(
+                      hintText: "USDT地址",
+                      inputController: addressController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return '请输入地址';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 6),
+                    child: Text(
+                      "NETWORK名称",
+                      style: messageText1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: secondaryTextFieldComponent(
+                      hintText: "Network地址",
+                      inputController: networkController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return '请输入地址';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 6),
+                    child: Text(
+                      "货币",
+                      style: messageText1,
+                    ),
+                  ),
+                  Text(
+                    "USDT",
+                    style: splashScreenTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        bottomNavigationBar: Container(
+          height: 84,
+          padding: EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+          alignment: Alignment.topCenter,
+          decoration: BoxDecoration(color: kMainWhiteColor, boxShadow: [
+            BoxShadow(
+              color: Color(0x1A000000),
+              offset: Offset(1, 0),
+              blurRadius: 2,
+              spreadRadius: 0,
+            ),
+          ]),
+          child: SizedBox(
+            width: double.infinity,
+            child: primaryButtonComponent(
+              text: "提交",
+              buttonColor: kMainYellowColor,
+              textStyle: buttonTextStyle,
+              onPressed: () {
+                setState(() {
+                  Get.back();
+                  Fluttertoast.showToast(
+                      msg: "已提交",
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      backgroundColor: kMainGreyColor,
+                      textColor: kThirdGreyColor);
+                });
+              },
             ),
           ),
         ),
