b5873091dcbb5e4f8e522c46ef067e5b795c156c
MindarkDaniel <jialexiong777@gmail.com>
2024-05-07 09:47:34 +0800

    create mission detail loading

--- part_time_app/lib/Components/Loading/missionDetailLoading.dart
+++ part_time_app/lib/Components/Loading/missionDetailLoading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:part_time_app/Constants/colorConstant.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -17,15 +18,31 @@ class _MissionDetailLoadingState extends State<MissionDetailLoading> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Shimmer.fromColors(
-            baseColor: kMainLoadingColor,
-            highlightColor: kSecondaryLoadingColor,
-            enabled: true,
-            child: Container(
-              height: 74,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: kMainWhiteColor),
+          Container(
+            height: 74,
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), color: kMainWhiteColor),
+            child: Shimmer.fromColors(
+              baseColor: kMainLoadingColor,
+              highlightColor: kSecondaryLoadingColor,
+              enabled: true,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: kMainWhiteColor,
+                    radius: 25,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                    height: 18,
+                    width: 100,
+                    color: kMainWhiteColor,
+                  ),
+                ],
+              ),
             ),
           ),
           SizedBox(
@@ -33,7 +50,7 @@ class _MissionDetailLoadingState extends State<MissionDetailLoading> {
           ),
           Container(
             height: 255,
-            padding: EdgeInsets.all(12),
+            padding: const EdgeInsets.all(12),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8), color: kMainWhiteColor),
             child: Shimmer.fromColors(
@@ -46,12 +63,39 @@ class _MissionDetailLoadingState extends State<MissionDetailLoading> {
                 children: [
                   Container(height: 26, width: 300, color: kMainWhiteColor),
                   SizedBox(
-                    height: 12,
+                    height: 2,
                   ),
                   Container(
                     height: 105,
-                    width: 327,
+                    width: double.infinity,
                     color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 98,
+                    color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 58,
+                        width: 180,
+                        color: kMainWhiteColor,
+                      ),
+                      Container(
+                        height: 29,
+                        width: 126,
+                        color: kMainWhiteColor,
+                      ),
+                    ],
                   )
                 ],
               ),
@@ -60,8 +104,88 @@ class _MissionDetailLoadingState extends State<MissionDetailLoading> {
           SizedBox(
             height: 12,
           ),
-          Row(
-            children: [],
+          Container(
+            height: 475,
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8), color: kMainWhiteColor),
+            child: Shimmer.fromColors(
+              baseColor: kMainLoadingColor,
+              highlightColor: kSecondaryLoadingColor,
+              enabled: true,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 23,
+                    width: 84,
+                    color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    height: 46,
+                    width: double.infinity,
+                    color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    height: 46,
+                    width: double.infinity,
+                    color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                        children: List.generate(
+                      3,
+                      (index) => Container(
+                        height: 100,
+                        width: 157,
+                        margin: EdgeInsets.only(right: 6),
+                        decoration: BoxDecoration(
+                            color: kMainWhiteColor,
+                            borderRadius: BorderRadius.circular(4)),
+                      ),
+                    )),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Container(
+                    height: 17,
+                    width: 79,
+                    color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                  Container(
+                    height: 46,
+                    width: double.infinity,
+                    color: kMainWhiteColor,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 157,
+                    margin: EdgeInsets.only(right: 6),
+                    decoration: BoxDecoration(
+                        color: kMainWhiteColor,
+                        borderRadius: BorderRadius.circular(4)),
+                  ),
+                ],
+              ),
+            ),
           )
         ],
       ),
